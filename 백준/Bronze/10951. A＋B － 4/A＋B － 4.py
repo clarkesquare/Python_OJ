647cab2a40b97dae405d795414ed160cad2d9e85
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-02-20 17:28:36 +0900

    [Bronze V] Title: A+B - 4, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -1,6 +1,8 @@
+import sys
+
 while True:
     try:
-        n, m = map(int, input().split())
+        n, m = map(int, sys.stdin.readline().split())
         print(n + m)
     except:
         break
