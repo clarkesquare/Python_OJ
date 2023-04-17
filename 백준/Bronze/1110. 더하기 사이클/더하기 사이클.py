715a65c372927cfd49ada4768d7a692ec98a392d
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-04-17 10:12:20 +0900

    [Bronze I] Title: 더하기 사이클, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -5,10 +5,10 @@ count = 0
 while True:
     count += 1
     if len(n) == 1:
-        n = str(0) + (n)
-    new = int(str(n[-1]) + str(int(n[0]) + int(n[-1]))[-1])
-    n = str(new)
+        n = str(0) + n
+    new = int(n[-1] + str(int(n[0]) + int(n[-1]))[-1])
     if new == original:
         break
+    n = str(new)
 
 print(count)
