4400022c391e03d185d42149ccbb031db9029a9d
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-05-30 09:51:13 +0900

    [Bronze I] Title: 박스, Time: 572 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9455. 박스/박스.py
@@ -0,0 +1,23 @@
+a = int(input())
+
+for _ in range(a):
+    box = []
+    cnt = 0
+    m, n = map(int, input().split())
+    for _ in range(m): # 이중 리스트 생성
+        box.append(list(map(int, input().split(' '))))
+    
+    for i in range(n):
+        temp = []
+        for j in range(m-1, -1, -1):
+            temp.append(box[j][i])
+        for k in range(m):
+            if temp[k] == 1:
+                for l in range(k):
+                    if temp[l] == 0:
+                        temp[l] = 1
+                        temp[k] = 0
+                        cnt += k-l
+                        break
+
+    print(cnt)
