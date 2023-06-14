3181ad2c08e02aaf7acaeb7e91f9553d80e069fc
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-06-14 09:16:49 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -3,13 +3,13 @@ answer = 0
 
 for _ in range(n):
     word = input()
-    temp = []
+    temp = ''
     for i in word:
         if i not in temp or temp[-1] == i:
-            temp.append(i)
+            temp += i
         else:
             break
-    if len(temp) == len(word):
+    if temp == word:
         answer += 1
 
 print(answer)
