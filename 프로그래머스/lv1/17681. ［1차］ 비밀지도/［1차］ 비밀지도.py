b49281145b0879705efc6b630986fc4935e68baf
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-06-26 23:24:04 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,22 @@
+def solution(n, arr1, arr2):
+    answer = []
+    temp = ''
+    
+    for i in range(len(arr1)):
+        arr1[i] = str(bin(arr1[i])[2::])
+        arr2[i] = str(bin(arr2[i])[2::])
+        if len(arr1[i]) != len(arr1):
+            arr1[i] = str('0' * (n - len(arr1[i]))) + arr1[i]
+        if len(arr2[i]) != len(arr2):
+            arr2[i] = str('0' * (n - len(arr2[i]))) + arr2[i]
+    
+    for i in range(n):
+        temp = ''
+        for j in range(len(arr1[0])):
+            if arr1[i][j] == '1' or arr2[i][j] == '1':
+                temp += '#'
+            else:
+                temp += ' '
+        answer.append(temp)
+        
+    return answer
