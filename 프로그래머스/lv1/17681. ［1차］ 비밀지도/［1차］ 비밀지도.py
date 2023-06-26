eca5390fd6ef3780e8593dd6c6d3500a61207f63
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-06-26 23:30:44 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -5,9 +5,9 @@ def solution(n, arr1, arr2):
     for i in range(len(arr1)):
         arr1[i] = str(bin(arr1[i])[2::])
         arr2[i] = str(bin(arr2[i])[2::])
-        if len(arr1[i]) != len(arr1):
+        if len(arr1[i]) != n:
             arr1[i] = str('0' * (n - len(arr1[i]))) + arr1[i]
-        if len(arr2[i]) != len(arr2):
+        if len(arr2[i]) != n:
             arr2[i] = str('0' * (n - len(arr2[i]))) + arr2[i]
     
     for i in range(n):
