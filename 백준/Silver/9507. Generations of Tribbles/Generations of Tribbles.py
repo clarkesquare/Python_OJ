5fbae73716218be094351e05196e901a0ad90d1b
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-07-17 17:36:33 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.py
+++ 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 for _ in range(int(input())):
     arr = [1, 1, 2, 4]
     n = int(input())
-    if n == 0 or n == 1 or n == 2 or n == 3:
+    if n <= 3:
         print(arr[n])
     else:
         for _ in range(n-3):
