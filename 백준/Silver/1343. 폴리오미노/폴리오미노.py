90ccf6422d1ddbd7d2bfbfb77734c4eded2d1231
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-08-16 08:49:40 +0900

    [Silver V] Title: 폴리오미노, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -5,7 +5,4 @@ polyomino = ['AAAA', 'BB']
 pattern = pattern.replace('XXXX', 'AAAA')
 pattern = pattern.replace('XX', 'BB')
 
-if 'X' in pattern:
-    print('-1')
-else:
-    print(pattern)
+print('-1' if 'X' in pattern else pattern)
