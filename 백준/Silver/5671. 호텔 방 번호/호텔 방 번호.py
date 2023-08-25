cfb56cdfca6148b8f1f403c2a24e23bc7175862d
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-08-25 11:24:53 +0900

    [Silver V] Title: 호텔 방 번호, Time: 1304 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5671. 호텔 방 번호/호텔 방 번호.py
+++ 백준/Silver/5671. 호텔 방 번호/호텔 방 번호.py
@@ -1,17 +1,17 @@
 while True:
     try:
         n, m = map(int, input().split())
-        rooms = list(map(str, range(n, m+1)))
-        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        answer = 0
-
-        for i in rooms:
-            for j in numbers:
-                if i.count(j) >= 2:
-                    break
-            else:
-                answer += 1
-
-        print(answer)
     except:
         break
+    rooms = list(map(str, range(n, m+1)))
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    answer = 0
+
+    for i in rooms:
+        for j in numbers:
+            if i.count(j) >= 2:
+                break
+        else:
+            answer += 1
+
+    print(answer)
