95b53119218ccf6381fe6d7245fa095feece6ae5
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-08-28 20:33:58 +0900

    [Silver V] Title: 라디오, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/3135. 라디오/라디오.py
+++ 백준/Silver/3135. 라디오/라디오.py
@@ -6,7 +6,7 @@ for i in range(int(input())):
     favorite.append(int(input()))
 
 for j in favorite:
-    if abs(j-b) < abs(a-b) and (answer == abs(a-b) or (abs(j-b)+1 < answer)):
+    if abs(j-b) < answer:
         answer = abs(j-b)+1
 
 print(answer)
