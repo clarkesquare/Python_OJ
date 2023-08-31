7669fc12242da1a23250d2ddba4fe7bfc923061a
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-08-31 09:44:07 +0900

    [Bronze I] Title: ROT13, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,14 @@
+upperlist = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] *2
+lowerlist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] *2
+answer = ''
+
+for i in input():
+    if i.isalpha():
+        if i.islower():
+            answer += lowerlist[lowerlist.index(i)+13]
+        else:
+            answer += upperlist[upperlist.index(i)+13]
+    else:
+        answer += i
+
+print(answer)
