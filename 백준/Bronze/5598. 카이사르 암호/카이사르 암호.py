d4672fd01d4a85b946bcc9dc37365b02ac57952b
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-09-08 09:13:00 +0900

    [Bronze II] Title: 카이사르 암호, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5598. 카이사르 암호/카이사르 암호.py
@@ -0,0 +1,7 @@
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+answer = ''
+
+for i in input():
+    answer += alphabet[(alphabet.index(i)+23)%26]
+
+print(answer)
