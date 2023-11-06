6202ad2f071f3f437de606b282a50bf28488578c
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-11-07 08:57:40 +0900

    [Bronze I] Title: Badminton, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/13216. Badminton/Badminton.py
@@ -0,0 +1,24 @@
+wins = []
+score = []
+cnt, n = 0, 0
+game = input()
+
+while True:
+    wins = []
+    while True:
+        if wins.count('A') == 21 or wins.count('B') == 21:
+            break
+
+        wins.append(game[n])
+        n += 1
+    
+    print(str(wins.count('A')) + '-' + str(wins.count('B')))
+    if wins.count('A') == 21:
+        score.append('A')
+    else:
+        score.append('B')
+
+    if score.count('A') == 2 or score.count('B') == 2:
+        break
+
+print('A' if score.count('A') > score.count('B') else 'B')
