5bbda1321ac9ef9ba57502d92d5a8a1513aa4e09
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-12-07 09:27:25 +0900

    [Bronze II] Title: Ресторан, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/23738. Ресторан/Ресторан.py
@@ -0,0 +1,9 @@
+russian_alphabets = [['B', 'E', 'H', 'P', 'C', 'Y', 'X'], ['v', 'ye', 'n', 'r', 's', 'u', 'h']]
+
+word = input()
+answer = ''
+
+for i in word:
+    answer += russian_alphabets[1][russian_alphabets[0].index(i)] if i in russian_alphabets[0] else i.lower()
+
+print(answer)
