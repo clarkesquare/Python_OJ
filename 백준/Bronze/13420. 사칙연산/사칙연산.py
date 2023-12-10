d576626bef3d682bc31f6dd6d2f9fba5a7e198c3
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-12-10 11:17:55 +0900

    [Bronze II] Title: 사칙연산, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/13420. 사칙연산/사칙연산.py
@@ -0,0 +1,18 @@
+calculate = []
+result = ''
+
+for _ in range(int(input())):
+    calculate = list(input().split())
+    calculate[0], calculate[2], calculate[4] = int(calculate[0]), int(calculate[2]), int(calculate[4])
+    result = ''
+    
+    if calculate[1] == '+':
+        result = calculate[0] + calculate[2]
+    elif calculate[1] == '-':
+        result = calculate[0] - calculate[2]
+    elif calculate[1] == '*':
+        result = calculate[0] * calculate[2]
+    else:
+        result = int(calculate[0] / calculate[2])
+    
+    print('correct' if result == calculate[4] else 'wrong answer')
