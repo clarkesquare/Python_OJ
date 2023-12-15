7aeca46e64cf68f0ea18fb2579d1462d80ec9027
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2023-12-15 09:10:36 +0900

    [Bronze II] Title: Fridge of Your Dreams, Time: 116 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11104. Fridge of Your Dreams/Fridge of Your Dreams.py
+++ 백준/Bronze/11104. Fridge of Your Dreams/Fridge of Your Dreams.py
@@ -1,10 +1,5 @@
 cola = ''
-answer = 0
 
 for _ in range(int(input())):
-    answer = 0
-    cola = input()
-    for i in range(len(cola)):
-        answer += int(cola[::-1][i]) * (2 ** i)
-    
-    print(answer)
+    cola = int('0b'+input(), 2)
+    print(cola)
