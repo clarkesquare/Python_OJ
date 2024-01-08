cfc5060b470d0b24d901224fac26aaadb47f6515
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2024-01-08 10:55:30 +0900

    [Bronze I] Title: 이 구역의 승자는 누구야?!, Time: 472 ms, Memory: 42564 KB -BaekjoonHub

---
+++ 백준/Bronze/20154. 이 구역의 승자는 누구야？！/이 구역의 승자는 누구야？！.py
@@ -0,0 +1,14 @@
+from string import ascii_uppercase
+
+alphabets = list(ascii_uppercase)
+score = [3, 2, 1, 2, 3, 3, 3, 3, 1, 1, 3, 1, 3, 3, 1, 2, 2, 2, 1, 2, 1, 1, 2, 2, 2, 1]
+answer = 0
+
+numbers = list(input())
+for i in numbers:
+    answer += score[alphabets.index(i)]
+
+if int(str(answer)[-1]) % 2 != 0:
+    print('I\'m a winner!')
+else:
+    print('You\'re the winner?')
