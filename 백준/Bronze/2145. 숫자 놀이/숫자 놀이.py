46582fbd22ac1970485f8f08165d9390119a048a
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2024-01-17 08:39:09 +0900

    [Bronze II] Title: 숫자 놀이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2145. 숫자 놀이/숫자 놀이.py
+++ 백준/Bronze/2145. 숫자 놀이/숫자 놀이.py
@@ -1,6 +1,6 @@
 while True:
     numbers = list(input())
-    if len(numbers[0]) == 1 and numbers[0] == '0':
+    if len(numbers) == 1 and numbers[0] == '0':
         break
         
     while len(numbers) != 1:
