369351fcb6f93b24044d366ac26c214e30990997
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2024-02-08 10:15:27 +0900

    [Bronze II] Title: Jumbled Words, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/7583. Jumbled Words/Jumbled Words.py
+++ 백준/Bronze/7583. Jumbled Words/Jumbled Words.py
@@ -1,9 +1,7 @@
 while True:
     words = list(input().split())
     if words[0] != '#':
-        answer = []
         for i in range(len(words)):
-            temp = ''
             if len(words[i]) >= 4:
                 words[i] = words[i][0] + words[i][-2:0:-1] + words[i][-1]
 
