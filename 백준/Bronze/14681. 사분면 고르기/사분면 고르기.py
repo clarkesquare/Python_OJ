bed51b63d98ef8aed774cea6727e5ca63b0e8a07
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2024-02-13 23:59:19 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -2,10 +2,10 @@ a = int(input())
 b = int(input())
 
 if a > 0 and b > 0:
-    print('1')
+    print("1")
 elif a < 0 and b > 0:
-    print('2')
+    print("2")
 elif a < 0 and b < 0:
-    print('3')
+    print("3")
 else:
-    print('4')
+    print("4")
