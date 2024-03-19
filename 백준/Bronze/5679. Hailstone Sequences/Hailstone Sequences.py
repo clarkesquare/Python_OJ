61ce695615873a5ebbedf25d5adda0e3de279142
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2024-03-19 15:11:17 +0900

    [Bronze II] Title: Hailstone Sequences, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5679. Hailstone Sequences/Hailstone Sequences.py
@@ -0,0 +1,17 @@
+while True:
+    n = int(input())
+    max = n
+    if n != 0:
+        while n != 1:
+            if n % 2 == 0:
+                n = n // 2
+            else:
+                n = (3 * n)+1
+
+            if n > max:
+                max = n
+        
+        print(max)
+    
+    else:
+        break
