6d2241d02d38287a1500e17b460e40b7394f6feb
Taewoong Clarke Kim <72212797+clarkesquare@users.noreply.github.com>
2024-03-29 09:55:30 +0900

    [Bronze II] Title: Hard to Believe, but True!, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6565. Hard to Believe， but True！/Hard to Believe， but True！.py
@@ -0,0 +1,15 @@
+a = ''
+b = ''
+answer = ''
+
+while True:
+    numbers = input()
+    if numbers != "0+0=0":
+        a, b = numbers.split("+")
+        b, c = b.split("=")
+        a, b, c = a[::-1], b[::-1], c[::-1]
+        print("True" if int(a) + int(b) == int(c) else "False")
+
+    else:
+        print("True")
+        break
