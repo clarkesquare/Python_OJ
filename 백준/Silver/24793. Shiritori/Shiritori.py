518d7c43396ada805c5f4a8c0ce5e0a197adaf19
Taewoong Kim <72212797+clarkesquare@users.noreply.github.com>
2024-08-15 10:34:48 +0900

    [Silver IV] Title: Shiritori, Time: 152 ms, Memory: 52280 KB -BaekjoonHub

---
+++ 백준/Silver/24793. Shiritori/Shiritori.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+pattern = {}
+tmp = ""
+answer = "Fair Game"
+
+for i in range(int(input())):
+    word = input().strip()
+    if tmp != "game over":
+        if i == 0: # 최초 셋팅
+            tmp = word
+            pattern[tmp] = ""
+        
+        else: # 2회차부터 검사 진행
+            if word[0] != tmp[-1] or word in pattern:
+                tmp = "game over"
+            
+            else:
+                pattern[word] = ""
+                tmp = word
+
+        if tmp == "game over" and answer == "Fair Game": # 게임 오버
+            answer = "Player 1 lost" if i % 2 == 0 else "Player 2 lost"
+    
+print(answer)
