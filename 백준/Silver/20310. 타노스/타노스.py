0ca394c821c840481a7118e0eef0fed6d61f02f5
Taewoong Kim <72212797+clarkesquare@users.noreply.github.com>
2024-09-24 11:52:08 +0900

    [Silver III] Title: 타노스, Time: 36 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,4 +1,18 @@
-word = input()
+word = list(input())
 answer = [word.count("0")//2, word.count("1")//2]
 
-print("0" * answer[0] + "1" * answer[1])
+# 앞에서부터 1 제거, 뒤에서부터 0 제거!!
+
+for _ in range(answer[1]):
+    for i in range(len(word)):
+        if word[i] == "1":
+            word[i] = ""
+            break
+
+for j in range(answer[0]):
+    for j in range(len(word)-1, 0, -1):
+        if word[j] == "0":
+            word[j] = ""
+            break
+
+print(*word, sep="")
