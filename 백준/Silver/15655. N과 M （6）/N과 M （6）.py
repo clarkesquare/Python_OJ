f16e16760084b58130cf7d79ea8fd61f7c8711ac
Taewoong Kim <72212797+clarkesquare@users.noreply.github.com>
2024-10-09 09:32:16 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,23 @@
+a, b = map(int, input().split())
+
+pattern = sorted(list(map(int, input().split())))
+answer = [[i] for i in pattern]
+tmp = []
+
+def nnm():
+    global pattern
+    global answer
+    tmp = []
+    for i in answer:
+        for j in pattern:
+            if i[-1] < j:
+                tmp.append(i + [j])
+
+    answer = tmp
+    return 0
+
+for _ in range(b-1):
+    nnm()
+
+for a in answer:
+    print(*a)
