ec390f2cac7aa07d87503895bb3e5b3ece59ddb5
Taewoong Kim <72212797+clarkesquare@users.noreply.github.com>
2024-11-09 17:49:09 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int n;
+    int a;
+    int b;
+    int i;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i ++)
+    {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+    }
+
+    return 0;
+}
