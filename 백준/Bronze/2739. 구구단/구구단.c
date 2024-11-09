491af50703868e385d67dd534124f41e7d00a27f
Taewoong Kim <72212797+clarkesquare@users.noreply.github.com>
2024-11-09 17:17:58 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int a, i;
+
+    scanf("%d", &a);
+
+    for (i = 1; i <= 9; i++) {
+        printf("%d * %d = %d\n", a, i, a * i);
+    }
+
+    return 0;
+}
