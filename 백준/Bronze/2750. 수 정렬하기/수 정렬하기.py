f544350c5792fb9cca93ad5a809a4dd0bae44e3d
Taewoong Kim <72212797+clarkesquare@users.noreply.github.com>
2024-12-18 16:43:14 +0900

    [Bronze II] Title: 수 정렬하기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,7 +1,8 @@
-x = int(input())
-num_list = []
-for i in range(x):
-    num_list.append(int(input()))
-num_list1 = sorted(num_list)
-for i in range(len(num_list)):
-    print(num_list1[i])
+n = int(input())
+numbers = []
+
+for _ in range(n):
+    numbers.append(int(input()))
+
+numbers.sort()
+print(*numbers, sep = "\n")
