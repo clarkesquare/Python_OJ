5fe89af16fc10fbad3c270734b5470d5bccfc9c2
Taewoong Kim <72212797+clarkesquare@users.noreply.github.com>
2024-12-21 18:39:35 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.cc
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -1,28 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int arr[101][101];
-    int max = 0;
-    int x, y;
-    int i = 0, j = 0;
+int main(void)
+{
+	int n, maximum = 0;
+	int x, y;
 
-    // arr1에 입력
-    for (int i = 0; i < 9; i++)
-        for (int j = 0; j < 9; j++)
-            cin >> arr[i][j];
+	for (int i = 1; i <= 9; i++)
+	{
+		for (int j = 1; j <= 9; j++)
+		{
+			cin >> n;
+			if (maximum <= n)
+			{
+				maximum = n;
+				x = i;
+				y = j;
+			}
 
-    for (int i = 0; i < 9; i++)
-        for (int j = 0; j < 9; j++)
-            if (max <= arr[i][j])
-            {
-                max = arr[i][j];
-                x = i + 1;
-                y = j + 1;
-            }
-    
-    cout << max << endl;
-    cout << x << " " << y;
+		}
+		cout << endl;
 
-    return 0;
+	}
+	cout << maximum << endl << x << " " << y;
+
+	return 0;
 }
