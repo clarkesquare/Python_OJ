fff4d5f2b29aad1936a4e41891e0c31b071919c0
Taewoong Kim <72212797+clarkesquare@users.noreply.github.com>
2024-12-24 00:50:56 +0900

    [Gold V] Title: 톱니바퀴 (2), Time: 112 ms, Memory: 34968 KB -BaekjoonHub

---
+++ 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.py
@@ -0,0 +1,55 @@
+from collections import deque
+
+gears = []
+pattern = []
+answer = 0
+
+# 처음 셋팅
+n = int(input())
+for _ in range(n):
+    pattern.append(deque(input()))
+
+for i in range(int(input())):
+    a, b = map(int, input().split())
+    left = b
+    right = b
+    leftcheck = pattern[a-1][-2]
+    rightcheck = pattern[a-1][2]
+    pattern[a-1].rotate(b) # 최초 변경
+
+    # 오른쪽 쭉쭉
+    changed = 1
+    for i in range(a, n):
+        if changed == 1:
+            if rightcheck != pattern[i][-2]:
+                rightcheck = pattern[i][2]
+                right *= -1
+                pattern[i].rotate(right)
+            
+            else:
+                changed = 0
+        
+        else:
+            break
+    
+    # 왼쪽 쭉쭉
+    changed = 1
+    for i in range(a-2, -1, -1):
+        if changed == 1:
+            if leftcheck != pattern[i][2]:
+                leftcheck = pattern[i][-2]
+                left *= -1
+                pattern[i].rotate(left)
+            
+            else:
+                changed = 0
+        
+        else:
+            break
+
+# 결과 출력
+for i in range(n):
+    if pattern[i][0] == "1":
+        answer += 1
+
+print(answer)
