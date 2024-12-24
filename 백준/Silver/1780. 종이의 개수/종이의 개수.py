8171f55f7c8212c5ae5d2ff2f7b065346df19774
Taewoong Kim <72212797+clarkesquare@users.noreply.github.com>
2024-12-25 01:43:06 +0900

    [Silver II] Title: 종이의 개수, Time: 1652 ms, Memory: 308120 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -0,0 +1,33 @@
+n = int(input())
+original = n
+pattern = []
+cnt = [0, 0, 0]
+
+for _ in range(n):
+    pattern.append(list(map(int, input().split())))
+
+while n != 0:
+    for i in range(0, original, n):
+        for j in range(0, original, n):
+            if pattern[i][j] != 'a':
+                tmp = []
+                for a in range(i, i + n):
+                    tmp += pattern[a][j:j+n]
+                
+                if tmp.count(tmp[0]) == n ** 2:
+                    if tmp[0] == -1:
+                        cnt[0] += 1
+                    
+                    elif tmp[0] == 0:
+                        cnt[1] += 1
+                    
+                    else:
+                        cnt[2] += 1
+                
+                    for a in range(i, i + n):
+                        for b in range(j, j+n):
+                            pattern[a][b] = 'a'
+    
+    n //= 3
+
+print(*cnt, sep="\n")
