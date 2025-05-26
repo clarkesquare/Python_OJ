6d63c80d4543548738585a8690563d75d32a4004
Taewoong Kim <72212797+clarkesquare@users.noreply.github.com>
2025-05-26 20:24:25 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
@@ -1,16 +1,12 @@
 tmp = 0
 numbers = []
 chk = 0
-
-for i in range(10):
-    tmp += int(input())
-    numbers.append(tmp)
-
 answer = 1000
 
 for i in range(10):
-    if abs(numbers[i] - 100) <= answer:
-        answer = abs(numbers[i] - 100)
-        chk = numbers[i]
+    tmp += int(input())
+    if abs(tmp - 100) <= answer:
+        answer = abs(tmp - 100)
+        chk = tmp
 
 print(chk)
